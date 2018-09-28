49655a8553ccbcffebef4bd7f93c57135335c83a
MOISES BARBOZA DE F JUNIOR <MBARBOZ@BRASILCENTER.COM.BR>
2018-09-28 14:08:38 -0300

    Primeira versão

---
+++ DapperExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+
+namespace DapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=dapperTest;Integrated Security=True");
+
+            conn.Open();
+
+            var users = conn.Query("select * from Usuario");
+
+            foreach (var usr in users)
+            {
+                Console.WriteLine("{0} - {1} - {2}", usr.Id, usr.Nome, usr.Idade);
+            }
+
+            conn.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
